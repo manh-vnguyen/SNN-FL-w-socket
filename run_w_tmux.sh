69e01cabcb4ff8e12e9e3c4762b09eebbedc13ae
mnguy126 <mnguy126@students.kennesaw.edu>
2024-03-28 14:39:07 +0000

    Update

--- run_w_tmux.sh
+++ run_w_tmux.sh
@@ -2,12 +2,17 @@
 
 # Initialize variables
 ACTION=""
+PREFIX=""
+EXTRA_ARGS=""
+NUM_CLIENTS=7
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         start|halt|cleanup|cleantmux) ACTION="$1"; shift ;;
-        --train_log_prefix) TRAIN_LOG_PREFIX="$2"; shift 2 ;;
+        --prefix) PREFIX="$2"; shift 2 ;;
+        --extra_args) EXTRA_ARGS="$2"; shift 2 ;;
+        --num_clients) NUM_CLIENTS="$2"; shift 2 ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
 done
@@ -40,46 +45,31 @@ halt_command_in_session() {
   fi
 }
 
-# Function to clean up all log files with the given prefix
-cleanup_logs() {
-    local prefix="$1"
-    echo "Cleaning up log files in plot_data/ with prefix $prefix..."
-    rm -f plot_data/"${prefix}"*.out
-    echo "Cleanup completed."
-}
-
 # Function to clean up all tmux sessions
 clean_tmux_sessions() {
-    tmux list-sessions | grep -E 'server_session|client_session_[0-9]+' | cut -d: -f1 | while read session_name; do
-        tmux kill-session -t "$session_name"
+    tmux list-sessions | grep -E "${PREFIX}server_session|${PREFIX}client_session_[0-9]+" | cut -d: -f1 | while read session_name; do
+        # tmux kill-session -t "$session_name"
         echo "Killed tmux session $session_name"
     done
 }
 
-NUM_CLIENTS=7
+
 
 case "$ACTION" in
   start)
-    server_command="echo Starting server; python3 server_w_socket.py"
-    start_or_send_command "server_session" "$server_command"
+    server_command="echo Starting server; python3 server_w_socket.py $EXTRA_ARGS"
+    start_or_send_command "$PREFIX-server_session" "$server_command"
     sleep 10
     for i in $(seq 0 $NUM_CLIENTS); do
-        client_command="echo Starting client $i; python3 client_w_socket.py --node-id $i"
-        start_or_send_command "client_session_$i" "$client_command"
+        client_command="echo Starting client $i; python3 client_w_socket.py --node-id $i $EXTRA_ARGS"
+        start_or_send_command "$PREFIX-client_session_$i" "$client_command"
     done
     ;;
   halt)
-    halt_command_in_session "server_session"
+    halt_command_in_session "$PREFIX-server_session"
     for i in $(seq 0 $NUM_CLIENTS); do
-      halt_command_in_session "client_session_$i"
+      halt_command_in_session "$PREFIX-client_session_$i"
     done
-    ;;
-  cleanup)
-    if [ -z "$TRAIN_LOG_PREFIX" ]; then
-      echo "train_log_prefix is required for cleanup action."
-      exit 1
-    fi
-    cleanup_logs "$TRAIN_LOG_PREFIX"
     ;;
   cleantmux)
     clean_tmux_sessions
